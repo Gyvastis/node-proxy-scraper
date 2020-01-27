1f3d9c1a1631a29939d357cfd20d73b36b4f2ef1
Vaidas <shibalba1@gmail.com>
2020-01-27 22:44:45 +0100

    add proxylists.net

--- src/providers/us-proxy.org.js
+++ src/providers/us-proxy.org.js
@@ -5,7 +5,7 @@ const scrapeIt = require('scrape-it');
 const Fetch = () =>
   fetch("https://www.sslproxies.org/", {
     "headers": {
-      // "user-agent": null
+      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
     },
     "method": "GET",
   })
