7d0eb133fa042db513b4cfb2f992db5ed28b5e68
Vaidas <shibalba1@gmail.com>
2020-01-28 19:12:57 +0100

    add merged output

--- src/command.js
+++ src/command.js
@@ -5,6 +5,11 @@ const domainSearch = require('./search');
 const writeOutput = data => {
   try {
     fs.writeFileSync('./output/output.json', JSON.stringify(data, null, 4));
+
+    const dataMerged = [];
+    data.map(dataItem => dataItem.proxies.map(proxy => dataMerged.push(proxy)))
+    console.log(dataMerged.length, dataMerged.filter(proxy => proxy.ip && proxy.ip.length > 0).length);
+    fs.writeFileSync('./output/output.merged.json', JSON.stringify(dataMerged, null, 4));
   } catch (err) {
     // An error occurred
     console.error(err);
@@ -14,7 +19,7 @@ const writeOutput = data => {
 domainSearch(
   process.argv[2] !== undefined ? process.argv[2] : null
 ).then(data => {
-  writeOutput(data);
   // console.log(data)
   console.log(util.inspect(data, false, null, true));
+  writeOutput(data);
 });
